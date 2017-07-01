850540adead10b14be4390a086a3b3f83aa4a8de
tomerlich <tomer.erlich2@gmail.com>
2017-07-01 10:34:42 -0700

    worked on the game class

--- core/src/com/myfirstgame/game/MyFirstGame.java
+++ core/src/com/myfirstgame/game/MyFirstGame.java
@@ -3,35 +3,37 @@ package com.myfirstgame.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.myfirstgame.game.States.GameStateManager;
+import com.myfirstgame.game.States.MenuState;
 
-import java.util.Stack;
 
 public class MyFirstGame extends ApplicationAdapter {
-	SpriteBatch batch;
-    Texture img;
-	Stack<GameStateManager> gsm;        //stack of states to determine what the screen will show
-	
+	public static final int WIDTH = 480;
+    public static final int HEIGHT = 800;
+    public static final float SCALE = 0.5f;
+
+    private SpriteBatch spriteBatch;
+    private GameStateManager gameStateManager;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-	    img = new Texture("badlogic.jpg");
+		spriteBatch = new SpriteBatch();
+        gameStateManager = new GameStateManager();
+        gameStateManager.push(new MenuState(gameStateManager));
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
     }
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        gameStateManager.update(Gdx.graphics.getDeltaTime());
+        gameStateManager.render(spriteBatch);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		spriteBatch.dispose();
 	}
 }
